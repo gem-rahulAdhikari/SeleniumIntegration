a792f9d8b707a7cb8021e4b146981492807b173b
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-07-19 15:52:39 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,16 +1,4 @@
 
-
-/**
- * Hello world!
- *
- */
-/**public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}*/
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
