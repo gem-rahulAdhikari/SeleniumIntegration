1a24cecb9ef71cb96a1e15685c219965c9f46696
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-07-31 15:58:44 +0530

    Text update from online input

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,1 +1,49 @@
-hello this is new code
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.Keys;
+
+ 
+
+public class App {
+    public static void main(String[] args) {
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+
+ 
+
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox");
+        // Create a new instance of the ChromeDriver
+        WebDriver driver = new ChromeDriver(options);
+System.out.println("Test1");
+        // Navigate to a webpage
+        driver.get("https://www.google.com");
+System.out.println("Test2");
+
+ 
+
+        // Find an input element and enter text
+        WebElement inputElement = driver.findElement(By.xpath("//textarea"));
+        inputElement.sendKeys("Selenium"+ Keys.ENTER);
+String x=driver.findElement(By.xpath("(//span[text()='Selenium'])[3]")).getText();
+System.out.println("Output: "+x);
+
+WebElement linkElement = driver.findElement(By.cssSelector("h3 a"));
+        linkElement.click();
+
+ 
+
+        // Perform other actions or assertions as needed
+System.out.println("Test3");
+        // Close the browser
+        driver.quit();
+    }
+}
