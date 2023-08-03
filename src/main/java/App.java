3f0d5eac00488cc0c536822ef1f6795829bf5332
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-08-03 15:37:38 +0530

    Text update from online input

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
         
+  try {  
       import org.openqa.selenium.By;
       import org.openqa.selenium.WebDriver;
       import org.openqa.selenium.WebElement;
@@ -8,9 +9,7 @@
 
       public class App {
           public static void main(String[] args) {
-             
               System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-
               ChromeOptions options=new ChromeOptions();
               options.addArguments("--headless");
               options.addArguments("start-maximized"); 
@@ -18,13 +17,41 @@
               options.addArguments("--disable-extensions"); 
               options.addArguments("--disable-dev-shm-usage"); 
               options.addArguments("--no-sandbox");
-              // Your editable code here
           
-             
-              
-                 
-            
+              System.out.println("hello");
               driver.quit();
+              String apiUrl = "https://us-east-1.aws.data.mongodb-api.com/app/application-0-awqqz/endpoint/addSeleniumResult";
+              URL url = new URL(apiUrl);
+              HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+              connection.setRequestMethod("POST");
+              connection.setRequestProperty("Content-Type", "application/json");
+              connection.setDoOutput(true);
+              String postData = "{\"output1\": " + x + "}";
+              try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
+                  outputStream.writeBytes(postData);
+                  outputStream.flush();
+              }
+              int statusCode = connection.getResponseCode();
+              String statusMessage = connection.getResponseMessage();
+              System.out.println("Status Code: " + statusCode);
+              System.out.println("Status Message: " + statusMessage);
+              if (statusCode == HttpURLConnection.HTTP_OK) {
+                  try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                      String line;
+                      StringBuilder response = new StringBuilder();
+                      while ((line = reader.readLine()) != null) {
+                          response.append(line);
+                      }
+                      System.out.println("Response: " + response.toString());
+                  }
+              } else {
+                  System.out.println("Error Response: " + statusCode);
+              }
+              connection.disconnect();
+          }
+          catch (Exception e) {
+            e.printStackTrace();
+        }
           }
       }
   
