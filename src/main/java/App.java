fa7f69e745a6dae27a2077a04e4053e68f41dd0c
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-08-03 08:23:29 +0530

    Text update from online input

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,30 @@
-[object HTMLPreElement]
+        
+      import org.openqa.selenium.By;
+      import org.openqa.selenium.WebDriver;
+      import org.openqa.selenium.WebElement;
+      import org.openqa.selenium.chrome.ChromeDriver;
+      import org.openqa.selenium.chrome.ChromeOptions;
+      import org.openqa.selenium.Keys;
+
+      public class App {
+          public static void main(String[] args) {
              
-              System.out.print("this is reuired code")
-[object HTMLPreElement]
+              System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+
+              ChromeOptions options=new ChromeOptions();
+              options.addArguments("--headless");
+              options.addArguments("start-maximized"); 
+              options.addArguments("disable-infobars");
+              options.addArguments("--disable-extensions"); 
+              options.addArguments("--disable-dev-shm-usage"); 
+              options.addArguments("--no-sandbox");
+              // Your editable code here
+          
+             
+              System.out.print("hello world");
+                 
+            
+              driver.quit();
+          }
+      }
+  
