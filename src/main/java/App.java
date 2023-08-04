a5a7fbcaeb537d12ade6801df216ca16ede508ed
Gem-Dipanshu-Kapoor <dipanshu.kapoor@geminisolutions.com>
2023-08-05 00:39:23 +0530

    testng update

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,70 +1,47 @@
-        
-      import java.io.BufferedReader;
-      import java.io.DataOutputStream;
-      import java.io.InputStreamReader;
-      import java.net.HttpURLConnection;
-      import java.net.URL;
-      import org.openqa.selenium.By;
-      import org.openqa.selenium.WebDriver;
-      import org.openqa.selenium.WebElement;
-      import org.openqa.selenium.chrome.ChromeDriver;
-      import org.openqa.selenium.chrome.ChromeOptions;
-      import org.openqa.selenium.Keys;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-      public class App {
-          public static void main(String[] args) {
-            try {  
-              System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-              ChromeOptions options=new ChromeOptions();
-              options.addArguments("--headless");
-              options.addArguments("start-maximized"); 
-              options.addArguments("disable-infobars");
-              options.addArguments("--disable-extensions"); 
-              options.addArguments("--disable-dev-shm-usage"); 
-              options.addArguments("--no-sandbox");
-          
-              WebDriver driver = new ChromeDriver(options);
-driver.get("https://www.google.com");
-WebElement inputElement = driver.findElement(By.xpath("//textarea"));
-inputElement.sendKeys("Selenium"+ Keys.ENTER);
-String x=driver.findElement(By.xpath("(//span[text()='Selenium'])[3]")).getText();
-System.out.println("Output: "+x);
-WebElement linkElement = driver.findElement(By.cssSelector("h3 a"));
-linkElement.click();
+public class App {
+    static WebDriver driver;
 
-              driver.quit();
-              String apiUrl = "https://us-east-1.aws.data.mongodb-api.com/app/application-0-awqqz/endpoint/addSeleniumResult";
-              URL url = new URL(apiUrl);
-              HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-              connection.setRequestMethod("POST");
-              connection.setRequestProperty("Content-Type", "application/json");
-              connection.setDoOutput(true);
-              String postData = "{\"output1\": " + x + "}";
-              try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
-                  outputStream.writeBytes(postData);
-                  outputStream.flush();
-              }
-              int statusCode = connection.getResponseCode();
-              String statusMessage = connection.getResponseMessage();
-              System.out.println("Status Code: " + statusCode);
-              System.out.println("Status Message: " + statusMessage);
-              if (statusCode == HttpURLConnection.HTTP_OK) {
-                  try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                      String line;
-                      StringBuilder response = new StringBuilder();
-                      while ((line = reader.readLine()) != null) {
-                          response.append(line);
-                      }
-                      System.out.println("Response: " + response.toString());
-                  }
-              } else {
-                  System.out.println("Error Response: " + statusCode);
-              }
-              connection.disconnect();
-          }
-          catch (Exception e) {
+    @BeforeMethod
+    public void configureDriver(){
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("start-maximized");
+        options.addArguments("disable-infobars");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterMethod
+    public void quitDriver(){
+        driver.quit();
+    }
+    @Test
+    public void run() {
+        try {
+            driver.get("https://www.google.com");
+            WebElement inputElement = driver.findElement(By.xpath("//textarea"));
+            inputElement.sendKeys("Selenium" + Keys.ENTER);
+            String x = driver.findElement(By.xpath("(//span[text()='Selenium'])[3]")).getText();
+            System.out.println("Output: " + x);
+            WebElement linkElement = driver.findElement(By.cssSelector("h3 a"));
+            linkElement.click();
+        } catch (Exception e) {
             e.printStackTrace();
         }
-          }
-      }
+    }
+}
   
