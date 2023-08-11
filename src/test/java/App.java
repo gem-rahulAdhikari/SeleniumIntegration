606ab4e8f20f7b09816164d9e97f97f3e66654a6
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-08-11 12:43:28 +0530

    Text update from online input

--- src/test/java/App.java
+++ src/test/java/App.java
@@ -1,47 +1,62 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+        
+      import java.io.BufferedReader;
+      import java.io.DataOutputStream;
+      import java.io.InputStreamReader;
+      import java.net.HttpURLConnection;
+      import java.net.URL;
+      import org.openqa.selenium.By;
+      import org.openqa.selenium.WebDriver;
+      import org.openqa.selenium.WebElement;
+      import org.openqa.selenium.chrome.ChromeDriver;
+      import org.openqa.selenium.chrome.ChromeOptions;
+      import org.openqa.selenium.Keys;
 
-public class App {
-    WebDriver driver;
-
-    @BeforeClass
-    public void configDriver(){
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("start-maximized");
-        options.addArguments("disable-infobars");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-
-        driver = new ChromeDriver(options);
-    }
-
-    @AfterClass
-    public void quitDriver(){
-        driver.quit();
-    }
-
-    @Test
-    public void run(){
-        try {
-            driver.get("https://www.google.com");
-            WebElement inputElement = driver.findElement(By.xpath("//textarea"));
-            inputElement.sendKeys("Selenium" + Keys.ENTER);
-            String x = driver.findElement(By.xpath("(//span[text()='Selenium'])[3]")).getText();
-            System.out.println("Output: " + x);
-            WebElement linkElement = driver.findElement(By.cssSelector("h3 a"));
-            linkElement.click();
-        } catch (Exception e) {
+      public class App {
+          public static void main(String[] args) {
+            try {  
+              System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+              ChromeOptions options=new ChromeOptions();
+              options.addArguments("--headless");
+              options.addArguments("start-maximized"); 
+              options.addArguments("disable-infobars");
+              options.addArguments("--disable-extensions"); 
+              options.addArguments("--disable-dev-shm-usage"); 
+              options.addArguments("--no-sandbox");
+          
+              System.out.print("hello");
+              driver.quit();
+              String apiUrl = "https://us-east-1.aws.data.mongodb-api.com/app/application-0-awqqz/endpoint/addSeleniumResult";
+              URL url = new URL(apiUrl);
+              HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+              connection.setRequestMethod("POST");
+              connection.setRequestProperty("Content-Type", "application/json");
+              connection.setDoOutput(true);
+              String postData = "{\"output1\": " + x + "}";
+              try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
+                  outputStream.writeBytes(postData);
+                  outputStream.flush();
+              }
+              int statusCode = connection.getResponseCode();
+              String statusMessage = connection.getResponseMessage();
+              System.out.println("Status Code: " + statusCode);
+              System.out.println("Status Message: " + statusMessage);
+              if (statusCode == HttpURLConnection.HTTP_OK) {
+                  try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                      String line;
+                      StringBuilder response = new StringBuilder();
+                      while ((line = reader.readLine()) != null) {
+                          response.append(line);
+                      }
+                      System.out.println("Response: " + response.toString());
+                  }
+              } else {
+                  System.out.println("Error Response: " + statusCode);
+              }
+              connection.disconnect();
+          }
+          catch (Exception e) {
             e.printStackTrace();
         }
-    }
-}
+          }
+      }
+  
