9e1692f8f87ab30a1a95b4ce86ea82ec6227f427
Gem-Dipanshu-Kapoor <dipanshu.kapoor@geminisolutions.com>
2023-08-11 16:16:55 +0530

    added shell script for bucket upload

---
+++ upload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Set your Google Cloud Storage bucket name and destination path in the bucket
+BUCKET_NAME="selenium-output"
+directory="/root/$0/test-output"
+file_extension=".html"
+filename=$(find "$directory" -maxdepth 1 -type f -name "*$file_extension" | grep "$file_extension")
+echo "Found file: $filename"
+echo $filename
+# Replace '/path/to/local/file.html' with the actual path to your HTML file on the VM
+gsutil cp /home/gemcodeeditor/ab echo -e "Before\tAfter" gs://${BUCKET_NAME}
