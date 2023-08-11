7e766ca4ad206665f64112cd0f831b69bec102fe
Dipanshu Kapoor <103283697+Gem-Dipanshu-Kapoor@users.noreply.github.com>
2023-08-11 16:53:48 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,9 +2,8 @@
 
 # Set your Google Cloud Storage bucket name and destination path in the bucket
 BUCKET_NAME="selenium-output"
-directory="/home/gemcodeeditor/$1/selenium_Integartion/test-output"
+directory="/root/$1/selenium_Integartion/test-output"
 file_extension=".html"
 filename=$(find "$directory" -maxdepth 1 -type f -name "*$file_extension" | grep "$file_extension")
 echo "Found file: $filename"
-# Replace '/path/to/local/file.html' with the actual path to your HTML file on the VM
 gsutil cp $filename gs://${BUCKET_NAME}
