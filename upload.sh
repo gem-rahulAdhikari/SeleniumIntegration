b988ab38e74f14a5866c81561d7e7abe903e666b
Dipanshu Kapoor <103283697+Gem-Dipanshu-Kapoor@users.noreply.github.com>
2023-08-11 16:33:17 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 
 # Set your Google Cloud Storage bucket name and destination path in the bucket
 BUCKET_NAME="selenium-output"
-directory="/root/$1/selenium_Integartion/test-output"
+directory="/home/gemcodeeditor/$1/selenium_Integartion/test-output"
 file_extension=".html"
 filename=$(find "$directory" -maxdepth 1 -type f -name "*$file_extension" | grep "$file_extension")
 echo "Found file: $filename"
