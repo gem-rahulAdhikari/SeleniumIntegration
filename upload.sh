50ddfbb3848d02acaa4cf0b820e2d0fd15addf6f
Dipanshu Kapoor <103283697+Gem-Dipanshu-Kapoor@users.noreply.github.com>
2023-08-11 16:24:39 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,6 +2,7 @@
 
 # Set your Google Cloud Storage bucket name and destination path in the bucket
 BUCKET_NAME="selenium-output"
+sudo su
 directory="/root/$1/test-output"
 file_extension=".html"
 filename=$(find "$directory" -maxdepth 1 -type f -name "*$file_extension" | grep "$file_extension")
