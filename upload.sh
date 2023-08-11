03e81522e4e665248dcb8c646143b7afd4a164ff
Dipanshu Kapoor <103283697+Gem-Dipanshu-Kapoor@users.noreply.github.com>
2023-08-11 16:24:17 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 
 # Set your Google Cloud Storage bucket name and destination path in the bucket
 BUCKET_NAME="selenium-output"
-directory="/root/$0/test-output"
+directory="/root/$1/test-output"
 file_extension=".html"
 filename=$(find "$directory" -maxdepth 1 -type f -name "*$file_extension" | grep "$file_extension")
 echo "Found file: $filename"
