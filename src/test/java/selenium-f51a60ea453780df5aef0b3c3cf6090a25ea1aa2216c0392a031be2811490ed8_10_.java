ad27fb06fa267249681c236a94b10e3ba4c0688c
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-08-14 13:36:52 +0530

    Creating new file with the desired name

---
+++ src/test/java/selenium-f51a60ea453780df5aef0b3c3cf6090a25ea1aa2216c0392a031be2811490ed8_10_.java
@@ -0,0 +1,20 @@
+import com.aventstack.extentreports.Status;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+import java.lang.*;
+import java.util.*;
+import java.awt.*;
+import java.math.*;
+import java.time.*;
+
+public class App extends driverConfig{
+
+    @Test
+                        
+System.out.print("selenium")
+    }
+                    
