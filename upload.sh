49a57ebf72c50a0d9e5a80ffb2dd989ef3641a14
Gem-Dipanshu-Kapoor <dipanshu.kapoor@geminisolutions.com>
2023-08-16 15:06:41 +0530

    delete all

--- upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set your Google Cloud Storage bucket name and destination path in the bucket
-BUCKET_NAME="selenium-output"
-directory="/root/$1/selenium_Integartion/test-output"
-file_extension=".html"
-filename=$(find "$directory" -maxdepth 1 -type f -name "*$file_extension" | grep "$file_extension")
-echo "Found file: $filename"
-gsutil cp $filename gs://${BUCKET_NAME}
