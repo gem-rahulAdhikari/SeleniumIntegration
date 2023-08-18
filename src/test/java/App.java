80110296065cfd454e562b1837a663c58aef2fc4
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-08-18 11:06:11 +0530

    Text update from online input

--- src/test/java/App.java
+++ src/test/java/App.java
@@ -12,10 +12,38 @@ import java.math.*;
 import java.time.*;
 
 public class App extends driverConfig{
-    static String reportName="Report_f51a60ea453780df5aef0b3c3cf6090a25ea1aa2216c0392a031be2811490ed8_16";
+    static String reportName="Report_f51a60ea453780df5aef0b3c3cf6090a25ea1aa2216c0392a031be2811490ed8_18";
 
-    @Test
-                        
-System.oiut.print("hello")
-    }
-                    
+    @Test";
+public void demo(){
+System.out.print("hello");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
