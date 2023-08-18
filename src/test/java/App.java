5dce63c8bfd1e8589069ca973b72dbc1444768e6
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-08-18 13:49:38 +0530

    Text update from online input

--- src/test/java/App.java
+++ src/test/java/App.java
@@ -12,11 +12,11 @@ import java.math.*;
 import java.time.*;
 
 public class App extends driverConfig{
-    static String reportName="Report_f51a60ea453780df5aef0b3c3cf6090a25ea1aa2216c0392a031be2811490ed8_22";
+    static String reportName="Report_f51a60ea453780df5aef0b3c3cf6090a25ea1aa2216c0392a031be2811490ed8_23";
 
-    @Test";
+    @Test
 public void demo(){
-  driver.get("https://www.google.com");
+driver.get("https://www.google.com");
             WebElement inputElement = driver.findElement(By.xpath("//textarea"));
             inputElement.sendKeys("Selenium");
             Actions action=new Actions(driver);
