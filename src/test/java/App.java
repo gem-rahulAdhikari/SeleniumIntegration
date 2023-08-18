f89b302a23955f5768ae0dd74d378648c851e4fe
Rahul Adhikari <Rahul.adhikari@Geminisolutions.com>
2023-08-18 12:20:56 +0530

    selenium_integration

--- src/test/java/App.java
+++ src/test/java/App.java
@@ -7,12 +7,12 @@ import org.testng.annotations.Test;
 
 import java.lang.*;
 import java.util.*;
-import java.awt.*;
+//import java.awt.*;
 import java.math.*;
 import java.time.*;
 
 public class App extends driverConfig{
-    static String reportName="report_key_count";
+    static String reportName="Report_f51a60ea453780df5aef0b3c3cf6090a25ea1aa2216c0392a031be2811490ed8_16";
 
     @Test
     public void run(){
--- src/test/java/driverConfig.java
+++ src/test/java/driverConfig.java
@@ -1,5 +1,8 @@
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,9 +13,10 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public abstract class driverConfig extends WebdriverEventListener {
     public static WebDriver driver;
@@ -28,7 +32,7 @@ public abstract class driverConfig extends WebdriverEventListener {
 
     @BeforeMethod
     public void setWebDriver() {
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:/Users/rahul.adhikari/Downloads/chromedriver_win32/chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("start-maximized");
@@ -64,10 +68,102 @@ public abstract class driverConfig extends WebdriverEventListener {
 
     public void mongoTransfer(String reportName) throws IOException {
         String userId=App.reportName.split("_")[1];
-        String url="http://g-codeeditor.el.r.appspot.com/editor?name="+userId;
+        System.out.println(userId);
+        String url="http://127.0.0.1:5000/editor?name="+userId;
+        System.out.println(url);
         String filePath = "src/test/java/App.java";
         String classContent = readClassFileAsString(filePath);
+        System.out.println(classContent);
+        System.out.println(reportName);
+        try {
+            // URL to send the PUT request
+            String apiUrl = "https://us-east-1.aws.data.mongodb-api.com/app/application-0-awqqz/endpoint/updateSeleniumSubmission";
 
+            // Create a URL object
+            URL url1 = new URL(apiUrl);
+
+            // Open a connection to the URL
+            HttpURLConnection connection = (HttpURLConnection) url1.openConnection();
+
+            // Set the request method to PUT
+            connection.setRequestMethod("PUT");
+
+            // Set the content type and other headers (if needed)
+            connection.setRequestProperty("Content-Type", "application/json");
+            // connection.setRequestProperty("Authorization", "Bearer YOUR_ACCESS_TOKEN");
+
+            // Enable output (to send data)
+            connection.setDoOutput(true);
+            String escapedClassContent = classContent.replace("\"", "\\\"")
+                    .replace("\n", "\\n")
+                    .replace("\r", "\\r");
+
+            String putData = "{\n" +
+                    "    \"filter\": {\n" +
+                    "        \"url\": \"" + url + "\"\n" +
+                    "    },\n" +
+                    "    \"SubmittedCode\":\""+ escapedClassContent +"\",\n" +
+                    "    \"Output\":\"" + reportName + "\"\n" +
+                    "}";
+
+
+            // Write the data to the connection's output stream
+            try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
+                outputStream.writeBytes(putData);
+                outputStream.flush();
+            }
+
+            // Get the HTTP response code
+            int statusCode = connection.getResponseCode();
+
+            // Get the HTTP response status message
+            String statusMessage = connection.getResponseMessage();
+
+            System.out.println("Status Code: " + statusCode);
+            System.out.println("Status Message: " + statusMessage);
+
+            if (statusCode == HttpURLConnection.HTTP_OK) {
+                // Read the response from the connection
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                    String line;
+                    StringBuilder response = new StringBuilder();
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    System.out.println("Response: " + response.toString());
+                    System.out.println(putData);
+                }
+            } else {
+                // Handle error response if needed
+                System.out.println("Error Response: " + statusCode);
+                System.out.println(putData);
+            }
+
+            // Disconnect the connection
+            connection.disconnect();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+//        RestAssured.baseURI = "https://us-east-1.aws.data.mongodb-api.com/";
+//        String jsonBody = "{\n" +
+//                "    \"filter\": {\n" +
+//                "        \"url\": \"" + url + "\"\n" +
+//                "    },\n" +
+//                "    \"SubmittedCode\":\"" + classContent + "\",\n" +
+//                "    \"Output\":\"" + reportName + "\"\n" +
+//                "}";
+//        Response response = RestAssured
+//                .given()
+//                .header("Content-type", "application/json")
+//                .contentType(ContentType.JSON)
+//                .body(jsonBody)
+//                .put("app/application-0-awqqz/endpoint/updateSeleniumSubmission")
+//                .then()
+//                .extract().response();
     }
     public static String readClassFileAsString(String filePath) throws IOException {
         StringBuilder content = new StringBuilder();
