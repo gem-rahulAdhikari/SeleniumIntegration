fdd9ede1a8957a3d20cdbe2860e713c667eac356
Dipanshu Kapoor <103283697+Gem-Dipanshu-Kapoor@users.noreply.github.com>
2023-08-18 12:43:56 +0530

    Update App.java

--- src/test/java/App.java
+++ src/test/java/App.java
@@ -12,7 +12,7 @@ import java.math.*;
 import java.time.*;
 
 public class App extends driverConfig{
-    static String reportName="";
+    static String reportName="test-report";
 
     @Test
     public void run(){
