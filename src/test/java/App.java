4f6eb7f7c150691fd1636b01c40e45750ac493f1
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-09-05 12:08:10 +0530

    Text update from online input

--- src/test/java/App.java
+++ src/test/java/App.java
@@ -1,28 +1,3 @@
-import com.aventstack.extentreports.Status;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.Test;
-
-import java.lang.*;
-import java.util.*;
-import java.awt.*;
-import java.math.*;
-import java.time.*;
-
-public class App extends driverConfig{
-    static String reportName="Report_19b7c34cce9380675f1393f62faa1c90616b6c5d2c064137855ba95262325e10_0";
-
-    @Test
-	public void demo(){
-driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_input_test");
-            WebElement iframeElement = driver.findElement(By.xpath("//iframe[@id='iframeResult']")); 
-            driver.switchTo().frame(iframeElement);
-            WebElement inputElement = driver.findElement(By.xpath("//input[@id='fname']"));
-            WebElement inputElement1 = driver.findElement(By.xpath("//input[@type='submit']"));
-            inputElement.sendKeys("Selenium");
-            Actions action=new Actions(driver);
-            action.click(inputElement1).build().perform();
+Report_bc391cc99c7742b220b0f9872cb26a26b33fc7d1d04722f77e2a974155990e89_0"Hello World");
 }
 }
