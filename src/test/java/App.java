dbefb69b847cb86b0d6717154671ab65fe442a5c
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-09-13 10:22:34 +0530

    Text update from online input

--- src/test/java/App.java
+++ src/test/java/App.java
@@ -15,6 +15,7 @@ public class App extends driverConfig{
     static String reportName="Report_ea3fb0747dfb0c6920bf615e71f9346dd4ee7569dfb9f840523545158ae9549c_0";
 
     @Test
+
 public void demo(){
 driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_input_test");
             WebElement iframeElement = driver.findElement(By.xpath("//iframe[@id='iframeResult']")); 
@@ -22,8 +23,7 @@ driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_input_test
             WebElement inputElement = driver.findElement(By.xpath("//input[@id='fname']"));
             WebElement inputElement1 = driver.findElement(By.xpath("//input[@type='submit']"));
             inputElement.sendKeys("Selenium");
-            Actions action=new Actions(driver);
-            action.click(inputElement1).build().perform();
+           
 
 
 }
