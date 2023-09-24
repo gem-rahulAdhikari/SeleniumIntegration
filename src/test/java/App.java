963a8010ffbd0f5d3e2c6a1d0d262aff080188e2
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-09-24 21:51:13 +0530

    Text update from online input

--- src/test/java/App.java
+++ src/test/java/App.java
@@ -5,23 +5,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
- 
-
 import java.lang.*;
 import java.util.*;
 import java.awt.*;
 import java.math.*;
 import java.time.*;
 
- 
-
 public class App extends driverConfig{
     static String reportName="Report_71d7729fcb7a8f201edff0ae55facead36f342438016d874710d91d074fc747d_0";
 
- 
-
     @Test
-    public void demo(){
+public void demo(){
 driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_input_test");
             WebElement iframeElement = driver.findElement(By.xpath("//iframe[@id='iframeResult']")); 
             driver.switchTo().frame(iframeElement);
@@ -30,5 +24,34 @@ driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_input_test
             inputElement.sendKeys("Selenium");
             Actions action=new Actions(driver);
             action.click(inputElement1).build().perform();
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
