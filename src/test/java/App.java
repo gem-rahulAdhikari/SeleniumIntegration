f80dc6d3b536f21d4e7b148d782c81be98dc0f67
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-10-17 12:28:37 +0530

    Text update from online input

--- src/test/java/App.java
+++ src/test/java/App.java
@@ -12,10 +12,9 @@ import java.math.*;
 import java.time.*;
 
 public class App extends driverConfig{
-    static String reportName="Report_71d7729fcb7a8f201edff0ae55facead36f342438016d874710d91d074fc747d_2";
+    static String reportName="Report_fbf7408f0ecae07265b30d14e645bd44416a87394a3a2855fbae5d31f10cbcbb_0";
 
     @Test
-
 public void demo(){
 driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_input_test");
             WebElement iframeElement = driver.findElement(By.xpath("//iframe[@id='iframeResult']")); 
@@ -28,6 +27,8 @@ driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_input_test
 
 
 }
+
+
 
 
 
