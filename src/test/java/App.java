c85811384918f8cddabf78454b426fa74a0f5b46
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-10-19 11:20:55 +0530

    Update App.java

--- src/test/java/App.java
+++ src/test/java/App.java
@@ -15,11 +15,16 @@ public class App extends driverConfig{
     static String reportName="Report_b092c8b27b1b4718bc1dfb4cc1eecdafadac8cee4cf9ceaca1a82699aa48db93_9";
 
     @Test
-public void demo() {
+public void demo() throws InterruptedException {
+
 driver.get("https://jewel.gemecosystem.com/");
-    WebElement loginButton = driver.findElement(By.xpath("//button//span[text()='Log in']"));
+    Thread.sleep(2000);
+    WebElement loginButton = driver.findElement(By.xpath("//span[text()='Log in']"));
+    Thread.sleep(2000);
     loginButton.click();
+    Thread.sleep(2000);
     WebElement userName = driver.findElement(By.xpath("//input[@id='usernameField']")); 
+    Thread.sleep(2000);
     userName.sendKeys("Rahul");
     extentTest.log(Status.PASS,driver.getCurrentUrl(),captureScreenshot());
 }
