442969ea0c85ff121e6a8cda993635ff08e8b951
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2023-10-19 10:30:45 +0530

    Update App.java

--- src/test/java/App.java
+++ src/test/java/App.java
@@ -12,13 +12,13 @@ import java.math.*;
 import java.time.*;
 
 public class App extends driverConfig{
-    static String reportName="Report_b092c8b27b1b4718bc1dfb4cc1eecdafadac8cee4cf9ceaca1a82699aa48db93_2";
+    static String reportName="Report_b092c8b27b1b4718bc1dfb4cc1eecdafadac8cee4cf9ceaca1a82699aa48db93_4";
 
     @Test
 public void demo(){
 driver.get("https://mis.geminisolutions.com/");
         System.out.println(driver.getCurrentUrl());
-        extentTest.log(Status.PASS,driver.getCurrentUrl()+x,captureScreenshot());
+        extentTest.log(Status.PASS,driver.getCurrentUrl(),captureScreenshot());
 }
 
 
