c4701e3b1780bd31c11e56acefb9b48035bf12d7
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2024-01-11 16:16:43 +0530

    Update YAML and Java files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,9 +4,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-
 
 import java.lang.*;
 import java.util.*;
@@ -15,20 +12,18 @@ import java.math.*;
 import java.time.*;
 
 public class App extends driverConfig{
-    static String reportName="Report_4a5d2e5676cf34734a533fe17b61b38dba979b836a374798302934e7b32ad777_0";
+    static String reportName="Report_1704969998_0";
 
     @Test
 public void demo(){
-    
 driver.get("https://www.google.com/");
      WebElement searchInput = driver.findElement(By.xpath("//textarea"));
     searchInput.sendKeys("selenium");
     searchInput.sendKeys(Keys.RETURN);
-     WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]"));
+     WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]")); 
      String fetchedTitle=title.getText();
-    System.out.println(fetchedTitle+" start2");      
-    
-    if("Selenium".equals(fetchedTitle))
+    System.out.println(fetchedTitle+" start2");
+     if("Selenium".equals(fetchedTitle))
      {
          extentTest.log(Status.PASS,"text matched successfully.",captureScreenshot());
      }
@@ -40,6 +35,7 @@ driver.get("https://www.google.com/");
      extentTest.log(Status.PASS,driver.getCurrentUrl(),captureScreenshot());
  
 }
+
 
 
 
