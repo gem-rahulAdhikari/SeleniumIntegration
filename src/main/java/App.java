d5199fc2409b97608347cb3098692393e53a0405
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2024-01-20 21:38:46 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,15 +17,34 @@ public class App extends driverConfig {
 
     @Test
     public void demo() {
-        RestAssured.baseURI = "https://reqres.in";
+        // RestAssured.baseURI = "https://reqres.in";
 
-        // Example GET request to retrieve a user (user with ID 1)
-        Response response = RestAssured.get("/api/users/1");
+        // // Example GET request to retrieve a user (user with ID 1)
+        // Response response = RestAssured.get("/api/users/1");
 
-        // Print the response status code
-        System.out.println("Status Code: " + response.getStatusCode());
+        // // Print the response status code
+        // System.out.println("Status Code: " + response.getStatusCode());
 
-        // Print the response body
-        System.out.println("Response Body: " + response.getBody().asString());
+        // // Print the response body
+        // System.out.println("Response Body: " + response.getBody().asString());
+            
+driver.get("https://www.google.com/");
+     WebElement searchInput = driver.findElement(By.xpath("//textarea"));
+    searchInput.sendKeys("selenium");
+    searchInput.sendKeys(Keys.RETURN);
+     WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]"));
+     String fetchedTitle=title.getText();
+    System.out.println(fetchedTitle+" start2");      
+    
+    if("Selenium".equals(fetchedTitle))
+     {
+         extentTest.log(Status.PASS,"text matched successfully.",captureScreenshot());
+     }
+    else
+     {
+         extentTest.log(Status.FAIL,"Failed to match text.",captureScreenshot());
+     }
+ 
+     extentTest.log(Status.PASS,driver.getCurrentUrl(),captureScreenshot());
     }
 }
