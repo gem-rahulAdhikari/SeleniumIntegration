17c6be6c5cef553ae0c30eac6f300fb98c82c0f4
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2024-01-20 22:30:51 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,10 +17,13 @@ public class App extends driverConfig {
 
     @Test
     public void demo() {
-        RestAssured.baseURI = "https://reqres.in";
-        Response response = RestAssured.get("/api/users/1");
-        System.out.println("Status Code: " + response.getStatusCode());
-        System.out.println("Response Body: " + response.getBody().asString());
+         driver.get("https://www.google.com/");
+        WebElement searchInput = driver.findElement(By.xpath("//textarea"));
+        searchInput.sendKeys("selenium")
+        searchInput.sendKeys(Keys.RETURN);
+        WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]"));
+        String fetchedTitle = title.getText();
+        System.out.println(fetchedTitle + " start2"); }
         
             
 
