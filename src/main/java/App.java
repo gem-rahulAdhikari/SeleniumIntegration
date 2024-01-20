e022efe6df48c9bd947e2eed664eabd23da68e00
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2024-01-20 22:16:29 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,14 +18,8 @@ public class App extends driverConfig {
     @Test
     public void demo() {
         RestAssured.baseURI = "https://reqres.in";
-
-        // Example GET request to retrieve a user (user with ID 1)
         Response response = RestAssured.get("/api/users/1");
-
-        // Print the response status code
         System.out.println("Status Code: " + response.getStatusCode());
-
-        // Print the response body
         System.out.println("Response Body: " + response.getBody().asString());
         
             
