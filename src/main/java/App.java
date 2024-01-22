cd228f1746205c55c3df64c705cc3ec7b5090603
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2024-01-22 16:30:11 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,8 +18,7 @@ public class App extends driverConfig {
 
     @Test
     public void demo() {
-        RestAssured.baseURI = "https://reqres.in";      
-        
+        RestAssured.baseURI = "https://reqres.in";        
         // Perform Rest Assured GET request
         Response response = RestAssured.get("/api/users/1");
 
