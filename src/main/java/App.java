66633786810d07cd6cfe461cedbdae57a7025401
Gem-Dipanshu-Kapoor <dipanshu.kapoor@geminisolutions.com>
2024-02-01 14:42:00 +0530

    restassured reporting

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,25 +19,10 @@ public class App extends driverConfig{
 
     @Test
 public void demo(){
-    
-driver.get("https://www.google.com/");
-     WebElement searchInput = driver.findElement(By.xpath("//textarea"));
-    searchInput.sendKeys("selenium");
-    searchInput.sendKeys(Keys.RETURN);
-     WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]"));
-     String fetchedTitle=title.getText();
-    System.out.println(fetchedTitle+" start2");      
-    
-    if("Selenium".equals(fetchedTitle))
-     {
-         extentTest.log(Status.PASS,"text matched successfully.",captureScreenshot());
-     }
-    else
-     {
-         extentTest.log(Status.FAIL,"Failed to match text.",captureScreenshot());
-     }
- 
-     extentTest.log(Status.PASS,driver.getCurrentUrl(),captureScreenshot());
+
+        RestAssured.baseURI = "https://betaapi.gemecosystem.com/gemEcosystemDashboard/actuator/health";
+        Response response = RestAssured.given().get().then().extract().response();
+        response.prettyPrint();
  
 }
 
