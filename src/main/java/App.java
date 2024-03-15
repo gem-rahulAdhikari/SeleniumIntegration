3b7ed359360abc98294e4da228599c3d76a3c85e
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2024-03-15 12:21:07 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,7 +15,6 @@ public class App extends driverConfig {
 
     @Test
     public void demo() {
-        
         driver.get("https://www.google.com/");
         WebElement searchInput = driver.findElement(By.xpath("//textarea"));
         searchInput.sendKeys("selenium");
