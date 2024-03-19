c632e54494547fc4628cb33c2f710617486b50d2
Gem-Dipanshu-Kapoor <dipanshu.kapoor@geminisolutions.com>
2024-03-19 20:57:14 +0530

    update app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,20 +1,9 @@
-import com.aventstack.extentreports.Status;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
 
-
-import java.lang.*;
-import java.util.*;
-import java.awt.*;
-import java.math.*;
-import java.time.*;
-
-public class App extends driverConfig{
+public class App extends driverConfig {
 
     @Test
     public void demo() {
@@ -24,5 +13,6 @@ public class App extends driverConfig{
         searchInput.sendKeys(Keys.RETURN);
         WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]"));
         String fetchedTitle = title.getText();
-        System.out.println(fetchedTitle + " start2"); }
+        System.out.println(fetchedTitle + " start2");
+    }
 }
