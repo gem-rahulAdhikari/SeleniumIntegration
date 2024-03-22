8d0a2c4c752291feec30e3880391107c852a1ea0
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2024-03-22 09:37:00 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -25,7 +25,7 @@ public class App extends driverConfig{
         searchInput.sendKeys(Keys.RETURN);
         WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]"));
         String fetchedTitle = title.getText();
-        System.out.println(fetchedTitle + " start2"); }
+        System.out.println(fetchedTitle + " start2"); 
 }
 
 
