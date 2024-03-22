c6f2212a158d2a98b6350dd9fba56234e766ba74
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2024-03-22 12:31:41 +0530

    Update YAML and Java files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,54 @@
+import com.aventstack.extentreports.Status;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
-public class App extends driverConfig {
+import java.lang.*;
+import java.util.*;
+import java.awt.*;
+import java.math.*;
+import java.time.*;
+
+public class App extends driverConfig{
+    static String reportName="Report_1711090896_0";
 
     @Test
-    public void demo() {
+public void demo() {
         driver.get("https://www.google.com/");
         WebElement searchInput = driver.findElement(By.xpath("//textarea"));
         searchInput.sendKeys("selenium");
         searchInput.sendKeys(Keys.RETURN);
         WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]"));
         String fetchedTitle = title.getText();
-        System.out.println(fetchedTitle + " start2");
-    
-    }
+        System.out.println(fetchedTitle + " start2"); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
