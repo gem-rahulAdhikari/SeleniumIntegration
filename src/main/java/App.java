abf9c37832021727b008f40b1da4663fad8a8c05
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2024-04-03 17:16:55 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class App extends driverConfig {
+    static String reportName="Report_1710855657_0";
 
     @Test
     public void demo() {
