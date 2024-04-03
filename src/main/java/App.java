324dc91ad28bf6ff724aebed8acb451239baa6bb
gem-rahulAdhikari <103276055+gem-rahulAdhikari@users.noreply.github.com>
2024-04-03 17:12:22 +0530

    Update YAML and Java files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,35 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.testng.annotations.Test;
-
-public class App extends driverConfig {
-
-    @Test
-    public void demo() {
-        driver.get("https://www.google.com/");
+Report_1712144537_0"https://www.google.com/");
         WebElement searchInput = driver.findElement(By.xpath("//textarea"));
         searchInput.sendKeys("selenium");
         searchInput.sendKeys(Keys.RETURN);
-        WebElement title = driver.findElement(By.xpath("(//h3[text()='Selenium'])[1]"));
-        String fetchedTitle = title.getText();
-        System.out.println(fetchedTitle + " start2");
-    
-    }
+       
+      
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
